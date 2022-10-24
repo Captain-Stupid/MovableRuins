812641eb4f3847e7085686beb39aab5cea6b1af5
Captain-Stupid <Backslash.Enter@gmail.com>
2022-10-24 15:08:17 -0500

    Add project files.

---
+++ MovableRuinsPlugin.cs
@@ -0,0 +1,111 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using CoreLib;
+using CoreLib.Submodules.CustomEntity;
+using CoreLib.Submodules.CustomEntity.Atributes;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace MovableRuins;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.le4fless.corelib")]
+[CoreLibSubmoduleDependency(new string[]
+    {
+        "LocalizationModule",
+        "CustomEntityModule"
+    })]
+public class MovableRuins : BasePlugin
+{
+    public const string GUID = "com.CaptainStupid.MovableRuins";
+    public const string NAME = "MovableRuins";
+    public const string VERSION = "1.0.0";
+
+    internal static MovableRuins Instance { get; private set; }
+    public static ManualLogSource Logger { get; private set; }
+
+    public override void Load()
+    {
+        Instance = this;
+        Logger = base.Log;
+
+        foreach (ObjectID ruin in Enum.GetValues(typeof(ObjectID)))
+        {
+            
+            if (ruin.ToString().ToLower().Contains("ruinspiece"))
+            {
+                Logger.LogInfo($"Adding localization for {ruin}");
+                CustomEntityModule.AddEntityLocalization(ruin,
+                $"Ancient Ruins {ruin.ToString().ToCharArray()[10]}",
+                "Whatever purpose it served is unknown, but it might make a good decoration!");
+            }
+
+        }
+
+        CustomEntityModule.RegisterModifications(typeof(MovableRuins));
+
+        var ourAssembly = Assembly.GetExecutingAssembly();
+        var resources = ourAssembly.GetManifestResourceNames();
+        foreach (var resource in resources)
+        {
+            if (!resource.EndsWith(".png"))
+                continue;
+
+            var stream = ourAssembly.GetManifestResourceStream(resource);
+
+            var ms = new MemoryStream();
+            stream.CopyTo(ms);
+            var resourceName = Regex.Match(resource, @"([a-zA-Z\d\-_]+)\.png").Groups[1].ToString();
+            ResourceManager.LoadSprite("MovableRuins", resourceName, ms.ToArray());
+        }
+
+        Log.LogInfo($"{NAME} is loaded!");
+    }
+
+    [EntityModification(ObjectID.RuinsPiece1)]
+    private static void RuinsPiece1(EntityMonoBehaviourData entity)
+    {
+        ModifyRuins(entity);
+
+    }
+
+    [EntityModification(ObjectID.RuinsPiece2)]
+    private static void RuinsPiece2(EntityMonoBehaviourData entity)
+    {
+        ModifyRuins(entity);
+    }
+
+
+    [EntityModification(ObjectID.RuinsPiece3)]
+    private static void RuinsPiece3(EntityMonoBehaviourData entity)
+    {
+        ModifyRuins(entity);
+    }
+
+    [EntityModification(ObjectID.RuinsPiece4)]
+    private static void RuinsPiece4(EntityMonoBehaviourData entity)
+    {
+        ModifyRuins(entity);
+    }
+
+    private static void ModifyRuins(EntityMonoBehaviourData entity)
+    {
+        Logger.LogInfo($"Entity name: {entity.name}");
+        GameObject gameObject = entity.gameObject;
+        gameObject.AddComponent<RequiresDrillCDAuthoring>();
+        gameObject.AddComponent<DestructibleObjectCDAuthoring>();
+        gameObject.AddComponent<PugAutomation.AutomatedMineableAuthoring>();
+        HealthCDAuthoring healthCDAuthoring = gameObject.GetComponent<HealthCDAuthoring>();
+        healthCDAuthoring.maxHealth = 12000;
+        healthCDAuthoring.startHealth = 12000;
+        entity.objectInfo.icon = ResourceManager.GetSprite($"MovableRuins.{entity.name}16x");
+        entity.objectInfo.smallIcon = ResourceManager.GetSprite($"MovableRuins.{entity.name}8x");
+        entity.objectInfo.objectType = ObjectType.PlaceablePrefab;
+        GameObject.Destroy(gameObject.GetComponent<HealthRegenerationCDAuthoring>());
+        GameObject.Destroy(gameObject.GetComponent<IndestructibleCDAuthoring>());
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# MovableRuins
+This is a mod for making Dirt biome ruins movable in Core Keeper, you must use drills to break the ruins.
+Sorry for the probably awful sprites :|
+
+## Note on multiplayer and save compatibility
+Everyone who plays with you will need this mod in order to join your server if you use this mod.
+
+# Installation
+### With Mod Manager
+
+Simply open the mod manager (if you don't have it install it [here](https://dsp.thunderstore.io/package/ebkr/r2modman/)), select **MovableRuins by CaptainStupid**, then **Download**.
+
+If prompted to download with dependencies, select `Yes`.
+Then just click **Start modded**, and the game will run with the mod installed.
+
+### Manually
+Install BepInEx Pack from [here](https://core-keeper.thunderstore.io/package/BepInEx/BepInExPack_Core_Keeper/)<br/>
+
+Unzip all files into `Core Keeper\BepInEx\plugins\MovableRuins/` (Create folder named `MovableRuins`)<br/>
+
+## Credits
+[ReMod.Core](https://github.com/RequiDev/ReMod.Core) for the awesome ResourceManager code.
+
+Roupiks#8368 for the sprites
+
+## Changelog
+<details>
+<summary>Changelog</summary>
+
+### v1.0.0
+- Initial Release
+</details>
